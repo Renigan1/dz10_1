e120178f952a74a9e26e1110aa9d97b476fe94a7
Александр Сапрыкин <ren1gan57@gmail.com>
2024-06-29 18:20:46 +0300

    все добавлено в commit

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-from src.masks import get_mask_account, get_mask_card_number
 from src.widget import get_data, mask_account_card
 
-print(get_mask_card_number('7000792289606361'))
+print(mask_account_card("Visa Platinum 7000 7922 8960 6361"))
 
-print(get_mask_account('73654108430135874305'))
+print(mask_account_card("Счет 73654108430135874305"))
 
-print(mask_account_card('Visa Platinum 7000 7922 8960 6361'))
-
-print(mask_account_card('Счет 73654108430135874305'))
-
-print(get_data('2018-07-11T02:26:18.671407'))
+print(get_data("2018-07-11T02:26:18.671407"))
--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,12 @@
 from src.masks import get_mask_account, get_mask_card_number
 
 
-
 def mask_account_card(num_card: str) -> str:
     """Функция общей маскировки карты и счета"""
-    if 'Счет' in num_card:
+    if "Счет" in num_card:
         account_number = num_card[5:]
         result = get_mask_account(account_number)
-        symbol_list1 = 'Счет'
+        symbol_list1 = "Счет"
     else:
         word_list = num_card.split()
         num_list = []
@@ -17,15 +16,16 @@ def mask_account_card(num_card: str) -> str:
                 symbol_list.append(word)
             elif word.isnumeric():
                 num_list.append(word)
-        symbol_list1 = ' '.join(symbol_list)
-        num_list1 = ''.join(num_list)
+        symbol_list1 = " ".join(symbol_list)
+        num_list1 = "".join(num_list)
         result = get_mask_card_number(num_list1)
-    return f'{symbol_list1} {result}'
+    return f"{symbol_list1} {result}"
+
 
 def get_data(date_output: str) -> str:
     """Функция преобразования даты"""
     data_result = date_output[:10]
-    data_list = data_result.split('-')
+    data_list = data_result.split("-")
     data_list.reverse()
-    result = '.'.join(data_list)
+    result = ".".join(data_list)
     return result
