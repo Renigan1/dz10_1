436e7b7db670d9d21c19bd423d540c3b18a10b32
Александр Сапрыкин <ren1gan57@gmail.com>
2024-06-29 11:44:44 +0300

    добавляю в коммит src и main.py

---
+++ src/masks.py
@@ -0,0 +1,10 @@
+def get_mask_card_number(mask_card_number: str) -> str:
+    """Функция, которая принимает на вход номер карты и возвращает ее маску"""
+    new_string = f"{mask_card_number[0:4]} {mask_card_number[4:6]} ** **** {mask_card_number[-4:]}"
+    return new_string
+
+
+def get_mask_account(mask_account: str) -> str:
+    """Функция, которая принимает на вход номер счета и возвращает его маску"""
+    new_string = f"** {mask_account[-4:]}"
+    return new_string
